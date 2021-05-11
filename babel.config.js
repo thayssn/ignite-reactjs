fbdaac70cd21e892f375c00bc9d6efa0eee18059
Thays <thays.santos@omeletecompany.com>
2021-05-11 00:49:15 -0300

    Configurando webpack

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
-module.exports = {
-    presets: [
+module.exports = function (api) {
+    api.cache(true);
+  
+    const presets = [
         '@babel/preset-env',
         '@babel/preset-react'
-    ]
-}
+    ];
+  
+    return {
+      presets,
+    };
+  }
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,2 @@
 import React from 'react';
-
-
-function App (){
-    return <h1>Hi, there!</h1>
-}
+import { App } from './App'
