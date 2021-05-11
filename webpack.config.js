b2fd7cdbbf71aac526bd812e82f4a49815994909
Thays <thays.santos@omeletecompany.com>
2021-05-11 00:57:43 -0300

    Estrutura react. Introduzindo o React no HTML.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'public', 'dist'),
         filename: 'bundle.js'
     },
     resolve: {
