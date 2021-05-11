2d242f88135ac5f655a9dc7e46a97afe50de5171
Thays <thays.santos@omeletecompany.com>
2021-05-11 01:08:55 -0300

    Configurando dev server e plugin para adicionar gerar o bundle e injetá-lo no HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,30 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
-        path: path.resolve(__dirname, 'public', 'dist'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     resolve: {
         extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html'),
+        })
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
     },
     module: {
         rules: [
             {
                 test: /\.jsx?$/,
                 include: [
-                    path.resolve(__dirname, "src")
+                    path.resolve(__dirname, "src"),
                 ],
                 exclude: [ /node_modules/],
                 loader: "babel-loader",
